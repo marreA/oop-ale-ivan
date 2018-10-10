5b183739ce50e20c7f975a9daef869c42ac35ff1
marreA <alemarrerodiaz@gmail.com>
2018-10-10 11:37:31 +0100

    Añadido .travis.yml, tests.js y la configuración de Gulp.

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+
+let gulp = require('gulp');
+let shell = require('gulp-shell');
+const mocha = require('gulp-mocha');
+
+gulp.task("default", ["test"]);
+gulp.task("test", () =>
+        gulp.src('./test/tests.js', {read: false})
+        .pipe(mocha({reporter: 'nyan'}))
+);
+
+gulp.task('docs', shell.task("asciidoctor docs/resumen.adoc -o docs/index.html"));
+
+gulp.task("pre-install", shell.task([
+    "sudo gem install asciidoctor"
+    ]));
