3333e47a894eaeb9208c5a7b3882f50ee0329f18
Iván García <alu0100693737@ull.edu.es>
2018-10-12 23:44:36 +0200

    Trabajando con tablas

--- src/Tabla.js
+++ src/Tabla.js
@@ -4,7 +4,7 @@ const UnderlinedCell = require('../src/underlinedcell');
 
 
   dataTable  = (data) => {
-
+    //console.log(data);
     var keys = Object.keys(data[0]);
     //console.log(keys);
     var categorias = keys.map(function(categoria) {
@@ -18,8 +18,33 @@ const UnderlinedCell = require('../src/underlinedcell');
         return new TextCell(i, TextCell.CENTER, nombre.toString());
       });
     });
-    
+
     return categorias.concat(datos);
     //console.log("Categorias \n " + categorias);
     //console.log("Datos \n " + datos);
+  };
+
+  drawTable = (data) => {
+    console.log(data.length);
+
+    drawLine = (data, numlinea) => {
+        console.log(data[numlinea]);
+        return data[numlinea];
+    }
+
+    drawLine(data, 6);
+  /*  var keys = Object.keys(data[0]);
+    console.log(keys);
+    var datos = data.map(function(fila) {
+      return keys.map(function(i) {
+        var nombre = fila[i];
+        console.log(i + " " + nombre);
+        return nombre;
+        //return new TextCell(i, TextCell.CENTER, nombre.toString());
+      });
+    });*/
+  };
+
+  createTable = (data) => {
+
   };
--- src/main.js
+++ src/main.js
@@ -3,6 +3,22 @@ const UnderlinedCell = require('../src/underlinedcell');
 const DataTable = require('../src/Tabla.js');
 //let textcell = new TextCell('plain', TextCell.LEFT, "HOOLA mundo");
 const input = require('../inputs/input.json');
+const fs = require('fs');
+let index = 3;
 
-var table = dataTable(input);
-console.log(table);
+
+lectorFichero = (nombreFichero) => {
+  return fs.readFileSync(nombreFichero, 'utf8');
+};
+
+
+if(index == process.argv.length) {
+  //console.log(process.argv[index -1]); //var table = dataTable(input);
+  //console.log(lectorFichero(process.argv[index -1]));
+  var table = dataTable(JSON.parse(lectorFichero(process.argv[index -1])));
+  //console.log(table);
+
+  drawTable(table);
+} else {
+    throw Error("A file must be specified!");
+}
