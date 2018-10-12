9c173e19a0a150afd6c7cb90920164f747b8635d
Iván García <alu0100693737@ull.edu.es>
2018-10-13 00:59:24 +0200

    Trabajando con lineas, formateo datos

--- src/Tabla.js
+++ src/Tabla.js
@@ -2,15 +2,32 @@ const TextCell = require('../src/textcell');
 const UnderlinedCell = require('../src/underlinedcell');
 
 
+colWidths = (filas) => {
+  return filas[0].map(function(_, i) {
+    return filas.reduce(function(max, row) {
+      return Math.max(max, row[i].minWidth);
+    }, 0);
+  });
+};
+
+colHeights = (filas) => {
+  return filas.map(function(row) {
+    return row.reduce(function(max, cell) {
+      return Math.max(max, cell.minHeight);
+    }, 0);
+  });
+};
 
   dataTable  = (data) => {
     //console.log(data);
     var keys = Object.keys(data[0]);
     //console.log(keys);
     var categorias = keys.map(function(categoria) {
-      return new UnderlinedCell(TextCell.LEFT, categoria);
+      return keys.map(function(categoria) {
+        return new UnderlinedCell(TextCell.LEFT, categoria);
+      });
     });
-
+    //console.log(categorias);
     var datos = data.map(function(fila) {
       return keys.map(function(i) {
         var nombre = fila[i];
@@ -26,13 +43,39 @@ const UnderlinedCell = require('../src/underlinedcell');
 
   drawTable = (data) => {
     console.log(data.length);
+    let width = colWidths(data);
+    let height = colHeights(data);
+
 
     drawLine = (data, numlinea) => {
-        console.log(data[numlinea]);
-        return data[numlinea];
+        var keys = Object.keys(data[numlinea][0]);
+        var pepe = data[numlinea].map(function(elemento) {
+            //console.log("Tam " + width + " " + height);
+            return elemento.draw(width[numlinea], height[numlinea]);
+        });
+        console.log(pepe);
+        //console.log(keys);
+        //console.log("Linea nume " + numlinea + " \n " + data[numlinea]);
+        return pepe;
     }
-
+    drawLine(data, 0);
+    drawLine(data, 1);
+    drawLine(data, 2);
+    drawLine(data, 3);
+    drawLine(data, 4);
+    drawLine(data, 5);
     drawLine(data, 6);
+    drawLine(data, 7);
+    drawLine(data, 8);
+    drawLine(data, 9);
+ };
+
+
+
+
+    //for (i = 0; i < data.length; i++) {
+    //    drawLine(data, i);
+    //  }
   /*  var keys = Object.keys(data[0]);
     console.log(keys);
     var datos = data.map(function(fila) {
@@ -43,7 +86,7 @@ const UnderlinedCell = require('../src/underlinedcell');
         //return new TextCell(i, TextCell.CENTER, nombre.toString());
       });
     });*/
-  };
+
 
   createTable = (data) => {
 
