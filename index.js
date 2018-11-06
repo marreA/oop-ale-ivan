4a600a020899be5ff4c9adf44d58957b72c09d1d
marreA <alemarrerodiaz@gmail.com>
2018-11-06 09:06:16 +0000

    Creando un index para la publicacion

---
+++ index.js
@@ -0,0 +1,15 @@
+let TextCell = require("./src/textcell.js");
+let RTextCell = require("./src/rtextcell.js");
+let UnderlinedCell = require("./src/underlinedcell.js");
+let StretchCell = require("./src/stretchcell.js");
+let DataTable = require("./src/tabla.js");
+let Registry = require("./registry-class.js");
+
+module.exports = {
+    TextCell, 
+    RTextCell,
+    UnderlinedCell, 
+    StretchCell,
+    DataTable,
+    Registry
+};
