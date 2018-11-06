8832d6f9b8e04ac251151c98f1957e4539aaba5a
marreA <alemarrerodiaz@gmail.com>
2018-11-06 09:40:09 +0000

    Solucionado un problema de sintaxis

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ let TextCell = require("./src/textcell.js");
 let RTextCell = require("./src/rtextcell.js");
 let UnderlinedCell = require("./src/underlinedcell.js");
 let StretchCell = require("./src/stretchcell.js");
-let DataTable = require("./src/tabla.js");
-let Registry = require("./registry-class.js");
+let DataTable = require("./src/Tabla.js");
+let Registry = require("./src/registry-class.js");
 
 module.exports = {
     TextCell, 
